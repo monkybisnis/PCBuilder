348498236481b4f1ee5fe00888ee29b2273e66b1
DavidJacob97 <david086.jacob@gmail.com>
2021-02-28 20:21:01 +0000

    getTotal cost
    get power consumption

--- src/PCBuilder/PC.java
+++ src/PCBuilder/PC.java
@@ -11,69 +11,6 @@ public class PC {
     private Storage storage;
     private Case aCase;
 
-    public CPU getCpu() {
-        return cpu;
-    }
-
-    public void setCpu(CPU cpu) {
-        this.cpu = cpu;
-    }
-
-    public GPU getGpu() {
-        return gpu;
-    }
-
-    public void setGpu(GPU gpu) {
-        this.gpu = gpu;
-    }
-
-    public Motherboard getMotherboard() {
-        return motherboard;
-    }
-
-    public void setMotherboard(Motherboard motherboard) {
-        this.motherboard = motherboard;
-    }
-
-    public Memory getMemory() {
-        return memory;
-    }
-
-    public void setMemory(Memory memory) {
-        this.memory = memory;
-    }
-
-    public Cooling getCooling() {
-        return cooling;
-    }
-
-    public void setCooling(Cooling cooling) {
-        this.cooling = cooling;
-    }
-
-    public PSU getPsu() {
-        return psu;
-    }
-
-    public void setPsu(PSU psu) {
-        this.psu = psu;
-    }
-
-    public Storage getStorage() {
-        return storage;
-    }
-
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
-
-    public Case getaCase() {
-        return aCase;
-    }
-
-    public void setaCase(Case aCase) {
-        this.aCase = aCase;
-    }
 
     public PC(CPU cpu, GPU gpu, Motherboard motherboard, Memory memory, Cooling cooling, PSU psu, Storage storage, Case aCase){
         this.cpu = cpu;
@@ -86,8 +23,11 @@ public class PC {
         this.aCase = aCase;
     }
 
-    public PC getPC(PC pc){
-        return pc;
+    public double getTotalCost(){
+        return 0;
     }
 
+    public double getPowerConsumption(){
+        return 0;
+    }
 }
