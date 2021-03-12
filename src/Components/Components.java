d333897e9ee0782b2f806a117ed7f3c9102f579d
norbi <17207738@studentmail.ul.ie>
2021-03-12 14:05:17 +0000

    attempt

--- src/Components/Components.java
+++ src/Components/Components.java
@@ -5,7 +5,7 @@ import PCBuilder.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Components implements PC {
+public class Components implements Component {
     private List<PC> components;
 
     public Components() {
@@ -18,5 +18,10 @@ public class Components implements PC {
 
     public void removeComponent(PC component) {
         components.remove(component);
+    }
+
+    @Override
+    public void showComponentProperties() {
+         // method to show properties of each comonent
     }
 }
