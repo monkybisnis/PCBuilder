0851517f9ea8239523185f95fdc5f023a1b68d4c
norbi <17207738@studentmail.ul.ie>
2021-03-12 14:28:57 +0000

    comonents+parts we need implementations for parts

--- src/PCBuilder/CPU.java
+++ src/PCBuilder/CPU.java
@@ -4,7 +4,7 @@ public class CPU extends Component {
     private double clockSpeed;
     private int cores;
     private String socket;
-    private Fan fan;
+    private Fan fans;
 
     CPU(String serial, String type, double price, double wattage, String brand, double clockSpeed, int cores, String socket, Fan fan) {
         super(serial, type, price, wattage, brand);
