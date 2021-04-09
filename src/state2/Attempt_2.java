8d832701265b0f0722dd3849463becfd00d34c2b
Rasto <17244382@studentmail.ul.ie>
2021-04-09 15:39:36 +0100

    after change of using outside framework this is new attempt for state machine

---
+++ src/state2/Attempt_2.java
@@ -0,0 +1,20 @@
+package state2;
+// this s my attempt ti do state machine for our project
+
+
+enum ClientStattes {
+    BRONZE_STATE,
+    SIVERR_STATE,
+    GOLD_STATE
+}
+
+enum Tranzission {
+    BRONZE_TO_SILVER, BRONZE_TO_GOLD,
+    SILVER_TO_GOLD, SILVER_TO_BRONZE,
+    GOLD_TO_SILVER, GOLD_TO_BRONZE
+}
+
+
+public class Attempt_2 {
+    // this is only temporary file for the gitGub
+}
