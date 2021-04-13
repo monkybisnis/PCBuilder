1a7af589e7270931ebcb0fe581a9ff21797cca62
Rasto <17244382@studentmail.ul.ie>
2021-04-13 17:06:47 +0100

    Edited Custom PC class

--- src/PCBuildInterface/CustomPC.java
+++ src/PCBuildInterface/CustomPC.java
@@ -1,37 +1,122 @@
 package PCBuildInterface;
 
 import Components.*;
-import Components.Part.CPU;
-import Components.Part.Case;
-import Components.Part.Motherboard;
-import Components.Part.PSU;
+import Components.Part.*;
 import PCBuilder.PC;
 
 public class CustomPC extends PC implements PC_Build {
-    private  Cases Cases= new Cases();
-    private  CPUs CPUs= new CPUs();
+    private  Cases cases = new Cases();
+    private  CPUs cpuS = new CPUs();
 
-    private  Motherboards  Motherboards= new Motherboards();
-    private  MemoryModules MemoryModules= new MemoryModules();
-    private  StorageComponents StorageComponents = new StorageComponents();
-    private GPUs Gpus=new GPUs();
-    private PSUs PSUs= new PSUs();
+    private  Motherboards motherboards = new Motherboards();
+    private  MemoryModules memoryModules = new MemoryModules();
+    private  StorageComponents storageComponents = new StorageComponents();
+    private GPUs gpus =new GPUs();
+    private PSUs psuS = new PSUs();
 
 
+    /**
+     * @param components main holder of whole PC object
+     *
+     * Constructor for Custom PC
+     */
+    public CustomPC(Components components){
+
+        for(Component component  : components.components){
+            if(component instanceof Cases){
+                this.cases = (Cases)component;
+            }
+            if(component instanceof CPUs){
+                this.cpuS = (CPUs) component;
+            }
+            if(component instanceof GPUs){
+                this.gpus = (GPUs) component;
+            }
+            if(component instanceof StorageComponents){
+                this.storageComponents = (StorageComponents) component;
+            }
+            if(component instanceof Motherboards){
+                this.motherboards = (Motherboards) component;
+            }
+            if(component instanceof MemoryModules){
+                this.memoryModules = (MemoryModules) component;
+            }
+            if(component instanceof PSUs){
+                this.psuS = (PSUs) component;
+            }
+        }
+
+        Case selectedCase               = addCase(cases);
+        CPU selectedCPU                 = addCPU(cpuS);
+        Motherboard selectedMotherBoard = addMotherBoard(motherboards,selectedCPU);
+        MemoryModules ramModule         = addRam(memoryModules);
+        StorageComponents disks         = addStorage(storageComponents);
+        PSU psu                         = addPSU(psuS);
+
+        setaCase(selectedCase);
+        setCpu(selectedCPU);
+        setMemory(ramModule);
+        setPsu(psu);
+        setStorage(disks);
+        setMotherboard(selectedMotherBoard);
+    }
+
+    /**
+     * @param cases
+     * @return
+     */
     @Override
     public Case addCase(Cases cases) {
-        return null;
+        Case selectedCase = cases.extract(0);
+
+        for(int i = 0; i < cases.size(); i++){
+            Case comparisonCase = cases.extract(i);
+            if(selectedCase.getPrice() > comparisonCase.getPrice()) {
+                selectedCase = comparisonCase;
+            }
+        }
+        return selectedCase;
     }
 
+    /**
+     * @param cpus
+     * @return
+     */
     @Override
     public CPU addCPU(CPUs cpus) {
-        return null;
+        CPU selectedCPU = cpus.extract(0);
+
+        for(int i=1 ;i< cpus.size(); i++){
+            CPU comparisonCPU= cpus.extract(i);
+
+            if(selectedCPU.getPrice()> comparisonCPU.getPrice()){
+                selectedCPU = comparisonCPU;
+            }
+        }
+        return selectedCPU;
     }
 
+    /**
+     * @param gpus
+     * @return
+     */
     @Override
     public GPUs addGPUs(GPUs gpus) {
-        return null;
+        GPUs chosenGpuS=new GPUs();
+        GPU selectedGPU = gpus.extract(0);
+
+        for(int i=1 ;i< gpus.size(); i++){
+            GPU comparisonGPU= gpus.extract(i);
+
+            if(selectedGPU.getClockSpeed() < comparisonGPU.getClockSpeed()){
+                selectedGPU=comparisonGPU;
+                chosenGpuS.add(selectedGPU);
+            }
+        }
+        return chosenGpuS;
     }
+
+    // TODO: progress from this line
 
     @Override
     public MemoryModules addRam(MemoryModules memories) {
