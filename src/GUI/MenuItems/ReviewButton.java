97bb1e1945f0d8784fca12917d343e6c6927c3b7
Rasto <17244382@studentmail.ul.ie>
2021-04-15 15:51:31 +0100

    Add to ReviewButton screen review menu button

---
+++ src/GUI/MenuItems/ReviewButton.java
@@ -0,0 +1,22 @@
+package GUI.MenuItems;
+
+import GUI.Command;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class ReviewButton extends MenuItem implements Command {
+    private Stage stage;
+
+    public ReviewButton(String label, Stage stage) {
+        super(label);
+        this.setOnAction(event -> this.execute());
+        this.stage = stage;
+    }
+    @Override
+    public void execute() {
+        displayLoginDialog();
+    }
+
+    private void displayLoginDialog() {
+    }
+}
