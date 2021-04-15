13737eff5b59255c2c3e853fdb90708ad8513044
norbi <17207738@studentmail.ul.ie>
2021-04-15 14:17:56 +0100

    Furhter seperation from Buttons to the UIViewModel(login)

--- src/GUI/MenuItems/LoginButton.java
+++ src/GUI/MenuItems/LoginButton.java
@@ -24,38 +24,10 @@ public class LoginButton extends MenuItem implements Command {
         super(label);
         this.setOnAction(event -> this.execute());
         this.stage = stage;
+
     }
 
-    public void displayLoginDialog() {
-        Dialog<Pair<String, String>> dialog = new Dialog<>();
 
-        Stage st = (Stage)dialog.getDialogPane().getScene().getWindow();
-        st.getIcons().add(new Image("/resources/case.png"));
-
-        dialog.setTitle("Login");
-
-        ButtonType loginButtonType = new ButtonType("Log in", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
-
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        TextField userName = new TextField();
-        userName.setPromptText("Username");
-        PasswordField password = new PasswordField();
-        password.setPromptText("Password");
-
-        vBox.getChildren().addAll(userName, password);
-        dialog.getDialogPane().setContent(vBox);
-
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == loginButtonType) {
-                return new Pair<>(userName.getText(), password.getText());
-            }
-            return null;
-        });
-        Optional<Pair<String, String>> result = dialog.showAndWait();
-        result.ifPresent(pair -> System.out.println("Username: " + pair.getKey() + " Password: " + pair.getValue()));
-    }
 
     @Override
     public void execute() {
--- src/GUI/MenuItems/UIViewModel.java
+++ src/GUI/MenuItems/UIViewModel.java
@@ -3,18 +3,52 @@ package GUI.MenuItems;
 import Components.Component;
 import Components.Part.Part;
 import javafx.scene.Node;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.util.Optional;
 
 public interface UIViewModel
 
 
 {
+
+    default void displayLoginDialog() {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+
+        Stage st = (Stage)dialog.getDialogPane().getScene().getWindow();
+        st.getIcons().add(new Image("/resources/case.png"));
+
+        dialog.setTitle("Login");
+
+        ButtonType loginButtonType = new ButtonType("Log in", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        TextField userName = new TextField();
+        userName.setPromptText("Username");
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+
+        vBox.getChildren().addAll(userName, password);
+        dialog.getDialogPane().setContent(vBox);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(userName.getText(), password.getText());
+            }
+            return null;
+        });
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+        result.ifPresent(pair -> System.out.println("Username: " + pair.getKey() + " Password: " + pair.getValue()));
+    }
+
 
     public default void setClickEvent(Label label, Part c) {
         ContextMenu contextMenu = new ContextMenu();
