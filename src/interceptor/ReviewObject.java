92657097ca64200df6ce0a694b565fe460f1f85c
Rasto <17244382@studentmail.ul.ie>
2021-04-19 14:40:53 +0100

    Created ReviewObject.java

---
+++ src/interceptor/ReviewObject.java
@@ -0,0 +1,57 @@
+package interceptor;
+
+public class ReviewObject {
+    private String itemID;
+    private String itemType;
+    private String userID;
+    private String review;
+
+    public ReviewObject(String itemID, String itemType, String userID, String review) {
+        this.itemID = itemID;
+        this.itemType = itemType;
+        this.userID = userID;
+        this.review = review;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewObject{" +
+                "itemID='" + itemID + '\'' +
+                ", itemType='" + itemType + '\'' +
+                ", userID='" + userID + '\'' +
+                ", review='" + review + '\'' +
+                '}';
+    }
+}
